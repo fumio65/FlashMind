1fb45d16ac3991bc4c0cba79a1592c016e7569de
Lhester <lhester@gmail.com>
2026-04-28 13:32:47 +0800

    feat: ErrorBoundary

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Providers } from './app/providers'
+import { Providers }      from './app/providers'
+import { ErrorBoundary }  from './components/shared/ErrorBoundary'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Providers />
+    <ErrorBoundary>
+      <Providers />
+    </ErrorBoundary>
   </StrictMode>
 )
